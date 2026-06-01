b0094863f2f359167f9d9cee1e228b3ca725448f
Shardul <mygithub.903@gmail.com>
2026-06-01 08:21:23 -0400

    fixed some uuid related issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./Components/Home/Home";
 import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
-import InstantConsultation from ".InstantConsultationBooking/InstantConsultationBooking";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultationBooking/InstantConsultation";
 
 function App() {
   return (
@@ -14,12 +14,18 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-            <Route path="/" element={<Landing_Page />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<Sign_Up />} />
-<Route path="/login" element={<Login />} />
+          {/* Landing page */}
+          <Route path="/" element={<Landing_Page />} />
 
+          {/* Home page */}
+          <Route path="/home" element={<Home />} />
+
+          {/* Auth routes */}
+          <Route path="/signup" element={<Sign_Up />} />
           <Route path="/login" element={<Login />} />
+
+          {/* Instant Consultation route */}
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
         </Routes>
       </BrowserRouter>
     </>
