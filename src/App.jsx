1974e033217b40abba3ff005d9b7b55402da6bcc
Shardul <mygithub.903@gmail.com>
2026-06-02 02:44:54 -0400

    added the notification componenet to the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,28 @@ import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
 import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultationBooking/InstantConsultation";
-import BookingConsultation from './Components/BookingConsultation';
+import BookingConsultation from "./Components/BookingConsultation";
+import Notification from "./Components/Notification/Notification"; 
+import DoctorCard from "./Components/DoctorCard/DoctorCard"; // ✅ Added
 
 function App() {
   return (
-    <>
+    <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          {/* Landing page */}
-          <Route path="/" element={<Landing_Page />} />
-
-          {/* Home page */}
-          <Route path="/home" element={<Home />} />
-
-          {/* Auth routes */}
-          <Route path="/signup" element={<Sign_Up />} />
-          <Route path="/login" element={<Login />} />
-
-          {/* Instant Consultation route */}
-          <Route path="/instant-consultation" element={<InstantConsultation />} />
-          <Route path="/booking-consultation" element={<BookingConsultation />} />
-
-        </Routes>
+        <Notification>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing_Page />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/signup" element={<Sign_Up />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/instant-consultation" element={<InstantConsultation />} />
+            <Route path="/booking-consultation" element={<BookingConsultation />} />
+            <Route path="/doctor-card" element={<DoctorCard />} /> {/* ✅ Fixed */}
+          </Routes>
+        </Notification>
       </BrowserRouter>
-    </>
+    </div>
   );
 }
 
